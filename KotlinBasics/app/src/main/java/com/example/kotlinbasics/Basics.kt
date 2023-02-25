9447d946945beba28fb06ccc7f657b3a06b6ee6c
thedarkarchitect <romanreynolds240@gmail.com>
2023-02-25 19:26:13 +0300

    first commit

---
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinbasics
+
+//fun main(){
+//    print("hello world!")
+//}
+
+fun main(args : Array<String>) {
+    println("Hello, World!")
+    name()
+
+}
+
+fun name(){
+    print("kevin")
+}
