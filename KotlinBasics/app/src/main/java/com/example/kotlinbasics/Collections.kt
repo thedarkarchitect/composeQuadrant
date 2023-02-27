c218c227284fa6b62e445fb6968e6839d7de5f81
thedarkarchitect <romanreynolds240@gmail.com>
2023-02-27 12:15:35 +0300

    final commit

---
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Collections.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinbasics
+
+fun main(){
+
+    //val numbers: IntArray = intArrayOf(1,2,3,4,5,6,7,8,9)
+    //val numbers = intArrayOf(1,2,3,4,5,6,7,8,9)
+    val numbers = arrayOf(1,2,3,4,5,6,7,8,9)
+//    print(numbers.contentToString())
+    for (element in numbers){
+        print(element)
+    }
+}
