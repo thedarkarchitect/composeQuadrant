31ab0e8ea6e7451dfbca54944bac0aaba3e96c02
thedarkarchitect <romanreynolds240@gmail.com>
2023-05-06 17:40:33 +0300

    var change again commit

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/Collections.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Collections.kt
@@ -6,7 +6,7 @@ fun main(){
     //val numbers = intArrayOf(1,2,3,4,5,6,7,8,9)
     val numbers = arrayOf(1,2,3,4,5,6,7,8,9)
 //    print(numbers.contentToString())
-    for (elements in numbers){
-        print(elements)
+    for (element in numbers){
+        print(element)
     }
 }
